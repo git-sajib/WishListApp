209d9c3a297baade5a9a1682b042df64ba2769f2
git-sajib <samiul.islam.unisel@gmail.com>
2024-08-12 19:04:18 +0600

    Adding A NavigationIcon to our AppBar

--- app/src/main/java/com/example/wishlistapp/AppBar.kt
+++ app/src/main/java/com/example/wishlistapp/AppBar.kt
@@ -2,10 +2,15 @@ package com.example.wishlistapp
 
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 
@@ -16,6 +21,19 @@ fun AppBarView(
     title: String,
     onBackNavClicked: () -> Unit = {}
 ){
+    val navigationIcon: (@Composable () -> Unit)? =
+        {
+            IconButton(onClick = { onBackNavClicked() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    tint = Color.White,
+                    contentDescription = null
+                )
+            }
+    }
+
+
+
     TopAppBar(title = { Text(text = title,
         color = colorResource(id = R.color.white),
         modifier = Modifier
@@ -24,5 +42,6 @@ fun AppBarView(
                       },
         elevation = 3.dp,
         backgroundColor = colorResource(id = R.color.app_bar_color),
+        navigationIcon = navigationIcon
         )
 }
