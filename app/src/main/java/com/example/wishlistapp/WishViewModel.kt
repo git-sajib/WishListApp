6af922e7c2572f76f0b98a2bcd17553d1111a68f
git-sajib <samiul.islam.unisel@gmail.com>
2024-08-15 12:22:30 +0600

    ViewModel and AddDetailView

--- app/src/main/java/com/example/wishlistapp/WishViewModel.kt
+++ app/src/main/java/com/example/wishlistapp/WishViewModel.kt
@@ -1,6 +1,20 @@
 package com.example.wishlistapp
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class WishViewModel: ViewModel() {
+    var wishTitleState by mutableStateOf("")
+    var wishDescriptionState by mutableStateOf("")
+
+    fun onWishTitleChange(newString: String){
+        wishTitleState = newString
+    }
+
+    fun onWishDescriptionChange(newString: String){
+        wishDescriptionState = newString
+    }
+
 }
