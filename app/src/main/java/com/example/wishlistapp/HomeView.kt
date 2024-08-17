05b1a9b215a7ba8b5c2bbfea3470095d95669272
git-sajib <samiul.islam.unisel@gmail.com>
2024-08-17 23:55:35 +0600

    Swipe to Delete Background

--- app/src/main/java/com/example/wishlistapp/HomeView.kt
+++ app/src/main/java/com/example/wishlistapp/HomeView.kt
@@ -2,7 +2,10 @@ package com.example.wishlistapp
 
 
 import android.widget.Toast
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,6 +20,7 @@ import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.rememberDismissState
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -24,6 +28,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -77,9 +83,25 @@ fun HomeView(
             
             SwipeToDismiss(
                 state = dismissState,
-                background = {},
-                directions = setOf(DismissDirection.StartToEnd, DismissDirection.EndToStart),
-                dismissThresholds = {FractionalThreshold(0.25f)},
+                background = {
+                    val color by animateColorAsState(
+                        if (dismissState.dismissDirection
+                            == DismissDirection.EndToStart) Color.Red else Color.Transparent
+                        ,label = ""
+                    )
+                    val alignment = Alignment.CenterEnd
+                    Box(
+                        Modifier.fillMaxSize().background(color).padding(horizontal = 20.dp),
+                        contentAlignment = alignment
+                    ){
+                        Icon(Icons.Default.Delete,
+                            contentDescription = "Delete Icon",
+                            tint = Color.White)
+                    }
+
+                },
+                directions = setOf(DismissDirection.EndToStart),
+                dismissThresholds = {FractionalThreshold(1f)},
                 dismissContent = {
                     WishItem(wish = wish) {
                         val id = wish.id
